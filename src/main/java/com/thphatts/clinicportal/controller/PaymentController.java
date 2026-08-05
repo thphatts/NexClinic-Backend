555270da1f9ed29dbd8fc3ad3387cc3dd7576879
thphatts <ngthanhphat0812@gmail.com>
2026-08-05 14:51:22 +0700

    feat(payment): add PaymentController and configure security permitAll endpoints

---
+++ src/main/java/com/thphatts/clinicportal/controller/PaymentController.java
@@ -0,0 +1,47 @@
+package com.thphatts.clinicportal.controller;
+
+
+import com.thphatts.clinicportal.common.ApiResponse;
+import com.thphatts.clinicportal.dto.request.CreatePaymentRequest;
+import com.thphatts.clinicportal.dto.response.PaymentResponse;
+import com.thphatts.clinicportal.entity.enums.PaymentMethod;
+import com.thphatts.clinicportal.service.AppointmentService;
+import com.thphatts.clinicportal.service.payment.PaymentService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("api/v1/payments")
+@RequiredArgsConstructor
+public class PaymentController {
+    private final PaymentService paymentService;
+    private final AppointmentService appointmentService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ApiResponse<PaymentResponse>> create (@Valid @RequestBody CreatePaymentRequest request, HttpServletRequest httpRequest) {
+        String ipAddress = httpRequest.getRemoteAddr();
+
+        PaymentResponse response = paymentService.createPayment(request.getAppointmentId(), request.getPaymentMethod(),ipAddress);
+        return ResponseEntity.ok(new ApiResponse<>(200, "Tạo giao dịch thành công", response));
+    }
+
+    @GetMapping("/vnpay-return")
+    public ResponseEntity<String> vnpayReturn(@RequestParam Map<String,String> params){
+        boolean success = "00".equals(params.get("vnp_ResponseCode"));
+        return ResponseEntity.ok(success ? "Thanh toán thành công! Vui lòng chờ hệ thống xác nhận."
+                : "Thanh toán thất bại hoặc bị hủy.");
+    }
+    @GetMapping("/vnpay-ipn")
+    public ResponseEntity<Map<String, String>> vnpayIpn(@RequestParam Map<String, String> params) {
+        boolean processed = paymentService.handleCallback(PaymentMethod.VNPAY, params);
+
+        return ResponseEntity.ok(processed
+                ? Map.of("RspCode", "00", "Message", "Confirm Success")
+                : Map.of("RspCode", "97", "Message", "Invalid Signature"));
+    }
+}
--- src/main/java/com/thphatts/clinicportal/entity/Product.java
+++ src/main/java/com/thphatts/clinicportal/entity/Product.java
@@ -17,8 +17,13 @@ import java.math.BigDecimal;
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private BigDecimal price;
     private String status;
 
